82d21d76a77df5f04ed40f4542257d92f26280d1
Oscarh71318 <oscarh71318@gmail.com>
2025-05-02 16:04:57 -0500

    Separar inserción de huéspedes en usuarios y huespedes en GuestRepositorio

--- repositorio/persisten/guestRepositorio.py
+++ repositorio/persisten/guestRepositorio.py
@@ -1,4 +1,3 @@
-
 from dominio.modelo.Guest import Guest
 from repositorio.conexion import Conexion
 
@@ -7,19 +6,34 @@ class GuestRepositorio:
         self.conexion = conexion
 
     def create_guest_repositorio(self, guest: Guest) -> None:
-        query = """
-            INSERT INTO guest (id, name, last_name, phone, mail, password, status, origin, occupation)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        values = (
-            guest.id,
-            guest.name,
-            guest.last_name,
-            guest.phone,
-            guest.mail,
-            guest.password,
-            guest.status,
-            guest.origin,
-            guest.occupation
-        )
-        self.conexion.execute_query(query, values)
+        self.conexion.connect()
+        try:
+            query_user = """
+                INSERT INTO usuarios (id, name, last_name, phone, mail, password, status)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+            user_data = (
+                guest.id,
+                guest.name,
+                guest.last_name,
+                guest.phone,
+                guest.mail,
+                guest.password,
+                guest.status
+            )
+            self.conexion.execute_query(query_user, user_data)
+
+            query_guest = """
+                INSERT INTO huespedes (id, origin, occupation)
+                VALUES (%s, %s, %s)
+            """
+            guest_data = (
+                guest.id,
+                guest.origin,
+                guest.occupation
+            )
+            self.conexion.execute_query(query_guest, guest_data)
+        except Exception as e:
+            print("Error al guardar huésped:", e)
+        finally:
+            self.conexion.disconnect()
